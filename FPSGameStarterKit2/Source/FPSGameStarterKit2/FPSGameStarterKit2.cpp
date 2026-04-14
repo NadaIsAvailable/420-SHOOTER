6d0b6ca9989884a89e89842ea91bcdffcd10edde
Kevin Zheng <kz3175@rit.edu>
2026-04-14 17:16:13 -0400

    temp

---
+++ FPSGameStarterKit2/Source/FPSGameStarterKit2/FPSGameStarterKit2.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "FPSGameStarterKit2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FPSGameStarterKit2, "FPSGameStarterKit2" );
